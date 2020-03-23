4fc1ae1b25a5895333aaa2256e98595ab7eb2575
Igor <iggor.malakhov@gmail.com>
2020-03-23 17:31:11 +0300

    Стартовый залив
    Тут весь проект разом

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.RegenButton = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(514, 55);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(58, 65);
+            this.richTextBox3.TabIndex = 0;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(757, 55);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(58, 65);
+            this.richTextBox4.TabIndex = 1;
+            this.richTextBox4.Text = "";
+            // 
+            // RegenButton
+            // 
+            this.RegenButton.Location = new System.Drawing.Point(795, 373);
+            this.RegenButton.Name = "RegenButton";
+            this.RegenButton.Size = new System.Drawing.Size(101, 66);
+            this.RegenButton.TabIndex = 2;
+            this.RegenButton.Text = "Восстановить!";
+            this.RegenButton.UseVisualStyleBackColor = true;
+            this.RegenButton.Click += new System.EventHandler(this.RegenButton_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(280, 55);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(58, 65);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(55, 55);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(56, 63);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Матрица Смежности Ребер";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Матрица Смежности Вершин";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(446, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(195, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Матрица Смежности вершин и ребер";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(745, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Список Вершин";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(908, 451);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.RegenButton);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Button RegenButton;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// В 1 текстбоксе записано что-то, заполить остальные
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+        class Graph
+        {
+            struct Vertex
+            {
+                int Data;
+                public Vertex(int NewDataValue)
+                {
+                    Data = NewDataValue;
+                }
+
+                public int getData() 
+                {
+                    return this.Data;
+                }
+
+                public void setData(int NewDataValue)
+                {
+                    this.Data = NewDataValue;
+                }
+            }
+
+            struct Edge
+            {
+                int Data;
+                Vertex TransmiterVertex, ReceiverVertex;
+                Edge(int NewDataValue)
+                {
+                    this.Data = NewDataValue;
+                    this.TransmiterVertex = null;
+                    this.ReceiverVertex =  null;
+                }
+                Edge(Vertex NewTransmiterVertex, Vertex NewReceiverVertex)
+                {
+                    this.Data = null;
+                    this.TransmiterVertex = NewTransmiterVertex;
+                    this.ReceiverVertex = NewReceiverVertex;
+                }
+                Edge(int NewDataValue, Vertex NewTransmiterVertex, Vertex NewReceiverVertex)
+                {
+                    this.Data = NewDataValue;
+                    this.TransmiterVertex = NewTransmiterVertex;
+                    this.ReceiverVertex = NewReceiverVertex;
+                }
+               int getData()
+                {
+                    return this.Data;
+                }
+
+                void setData(int NewDataValue)
+                {
+                    this.Data = NewDataValue;
+                }
+
+                Vertex getTransmiterVertex()
+                {
+                    return this.TransmiterVertex;
+                }
+                void setTransmiterVertex(Vertex NewTransmiterVertex)
+                {
+                    this.TransmiterVertex = NewTransmiterVertex;
+                }
+
+                Vertex getReceiverVertex()
+                {
+                    return this.ReceiverVertex;
+                }
+                void setReceiverVertex(Vertex NewReceiverVertex)
+                {
+                    this.ReceiverVertex = NewReceiverVertex;
+                }
+
+            }
+
+            private List<Vertex> VertexList = null; //Список Вершин
+            private List<Edge> EdgeList = null; // Список ребер
+
+            private int[,] MatrixOfVertexes = null; //Вершины
+            private int[,] MatrixOfEdges = null; //Ребра
+            private int[,] MatrixOfVertexesAndEdges = null; // оба
+
+            void addVertexToList(Vertex VertexToAdd)
+            {
+                VertexList.Add(VertexToAdd);
+            }
+
+            Vertex getVertexFromLList(int DataToFind)
+            {
+                foreach(Vertex Vert in VertexList)
+                {
+                    if (Vert.getData() == DataToFind)
+                    {
+                        return Vert;
+                    }
+                }
+                return (new Vertex(-100));
+            }
+            
+            void deleteVertexFromList(int DataToDelete)
+            {
+                foreach(Vertex Vert in VertexList)
+                {
+                    if (Vert.getData() == DataToDelete)
+                    {
+                        VertexList.Remove(Vert);
+                    }
+                }
+            }
+
+        }
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IfText1()
+        {
+
+        }
+        private void IfText2()
+        {
+
+        }
+        private void IfText3()
+        {
+
+        }
+        private void IfText4()
+        {
+
+        }
+
+        private void RegenButton_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text != "") IfText1();
+            if (richTextBox2.Text != "") IfText2();
+            if (richTextBox3.Text != "") IfText3();
+            if (richTextBox4.Text != "") IfText4();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
