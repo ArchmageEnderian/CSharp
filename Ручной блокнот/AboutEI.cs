8f4f15ee2a0fafd7b6eea918a92123713e24be74
Igor <iggor.malakhov@gmail.com>
2020-04-10 10:50:49 +0300

    Changed AboutEI.cs
    Changed AboutEI_Load function for lecturer

--- Ручной блокнот/AboutEI.cs
+++ Ручной блокнот/AboutEI.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +19,13 @@ namespace Ручной_блокнот
 
         private void AboutEI_Load(object sender, EventArgs e)
         {
-            richTextBox1.AppendText("Ручной блокнот создан Archmage『 Enderian 』(Малахов Игорь Дмитриевич)\nСпециально для (Могут сделать все по желанию, помним про бонус)\nХейтеры скажут фотошоп, я скажу - плагиат оригинального блокнота");
+            System.Drawing.Drawing2D.GraphicsPath myPath = new System.Drawing.Drawing2D.GraphicsPath();
+            // треугольная форма
+            myPath.AddPolygon(new Point[] { new Point(7, 7), new Point(this.Width-7, 7), new Point(this.Width-7, this.Height-8) });
+
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
+            richTextBox1.AppendText("                                                      Ручной блокнот создан Archmage『 Enderian 』\n                                                                    (Малахов Игорь Дмитриевич)\n                                                           Могут сделать все по желанию,\n                                                                             помним про бонус \n                                                                                       Хейтеры скажут фотошоп,\n                                                                                                              я скажу - \n                                                                                           плагиат оригинального\n                                                                                                          блокнота =D");
         }
     }
 }
