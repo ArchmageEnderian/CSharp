20511469204e7c0d1c48b03dada06683f46dd61a
『 Enderian 』 <iggor.malakhov@gmail.com>
2020-04-12 14:32:07 +0300

    Added logic to functions
    Added 2 functions: situation1 & situation2 save functions for user

--- База телефонов/База телефонов/Form1.cs
+++ База телефонов/База телефонов/Form1.cs
@@ -12,45 +12,36 @@ namespace База_телефонов
 {
     public partial class Form1 : Form
     {
-        private bool Format = false;
-        private string[] Mas = new string[1000];
+        private string[] masString = new string[1000];
+        private int j = 0;
         public Form1()
         {
             InitializeComponent();
         }
-        private void Situation1() // для +7-***-***-**-**
+        private void Situation1(string str, int counter) // для +7-***-***-**-**
         {
-            string UltraString = richTextBox1.Text;
-            for (int i = 0; i < UltraString.Length - 1; i++)
-            {
-                if (UltraString[i] == '+' && UltraString[i+1] == '7')
-                {
-
-
-                }
-            }
-
-
-
-
-
-            MessageBox.Show("Сохранение прошло успешно!", "Успешно!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            masString[j] += "+7" + "-" + str[counter + 3] + str[counter + 4] + str[counter + 5] + "-" + str[counter + 7] + str[counter + 8] + str[counter + 9] + str[counter + 10] + str[counter + 11] + str[counter + 12] + str[counter + 13] + str[counter + 14] + str[counter + 15];
+            j++;
         }
-        private void Situation2() // для 8**********
+        private void situation2(string str, int counter) // для 8**********
         {
-
+            masString[j] += "8" + str[counter + 3] + str[counter + 4] + str[counter + 5] + str[counter + 7] + str[counter + 8] + str[counter + 9] + str[counter + 11] + str[counter + 12] + str[counter + 14] + str[counter + 15];
+            j++;
         }
 
         private void ButtonChange_Click(object sender, EventArgs e)
         {
-            if (checkBox.Checked == true)
+            string ultraString = richTextBox1.Text;
+            for (int i = 0; i < ultraString.Length - 1; i++)
             {
+                if (ultraString[i] == '+' && ultraString[i + 1] == '7')
+                {
+                    if (checkBox.Checked == false) Situation1(ultraString, i);
+                    else situation2(ultraString, i);
 
+                }
             }
-            else
-            {
-                Situation1();
-            }
+            MessageBox.Show("Сохранение прошло успешно!", "Успешно!", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void CheckBox_CheckedChanged(object sender, EventArgs e)
