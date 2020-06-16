cd8387632d929ccdc2cdc2a692e1ad6cb9df2f7b
『 Enderian 』 <iggor.malakhov@gmail.com>
2020-06-16 20:39:05 +0300

    Смена проектов
    Причина - не робил MySQL

---
+++ Экзамен/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            string connectionString = "server=localhost;user=root;database=inda;password=0000;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            /*
+            string sql = "";
+            MySqlCommand command = new MySqlCommand(sql, connection);
+               
+            command.ExecuteNonQuery();
+            connection.Close();
+            */
+        }
+    }
+}
