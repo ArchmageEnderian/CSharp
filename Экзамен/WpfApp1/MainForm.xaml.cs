342e62150b4023baf9e912ee7d6294b790bd2033
『 Enderian 』 <iggor.malakhov@gmail.com>
2020-06-17 03:05:12 +0300

    Малая пересборка

--- Экзамен/WpfApp1/MainForm.xaml.cs
+++ Экзамен/WpfApp1/MainForm.xaml.cs
@@ -25,6 +25,8 @@ namespace WpfApp1
             InitializeComponent();
             SQL_Class.SQL_OpenServer();
         }
+        private string sql;
+        private bool factor = false;
         private void MainForm_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
 
@@ -32,8 +34,7 @@ namespace WpfApp1
 
         private void BlueSide(object sender, RoutedEventArgs e)
         {
-            
-            
+          factor = true;
         }
         private void SaveFile(object sender, RoutedEventArgs e)
         {
@@ -46,10 +47,35 @@ namespace WpfApp1
         }
         private void ProcLoad(object sender, RoutedEventArgs e)
         {
+            List<string> data = new List<string>();
+            data.Add("Выберете процессор");
             var combo = sender as ComboBox;
+            sql = "SELECT * FROM `processor`";
+            MySqlCommand command = new MySqlCommand(sql, SQL_Class.connection);
+            MySqlDataReader reader = command.ExecuteReader();
+            combo.SelectedIndex = 0;
+            while (reader.Read())
+            {
+                data.Add(reader[1].ToString() + " " + reader[2].ToString());
+            }
+            combo.ItemsSource = data;
+            reader.Close();
+        }
+
+        private void Proс_Selection(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = sender as ComboBox;
+            string name = selected.SelectedItem as string;
+            string[] stringer = name.Split(' ');
+            if (factor)
+            {
+                sql = "SELECT * FROM `processor` WHERE " + stringer[0];
+                MySqlCommand nextCommand = new MySqlCommand(sql, SQL_Class.connection);
+                MySqlDataReader reader = nextCommand.ExecuteReader(); 
+            }
 
         }
         /* TY thx = new TY();
-   thx.Show(); */
+thx.Show(); */
     }
 }
