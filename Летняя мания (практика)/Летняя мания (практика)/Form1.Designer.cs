aa5261da9e82e73d92868faf70c0996bbded57aa
『 Enderian 』 <iggor.malakhov@gmail.com>
2020-07-11 18:11:53 +0300

    Летняя практика - Начало

---
+++ Летняя мания (практика)/Летняя мания (практика)/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Летняя_мания__практика_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AtextBox = new System.Windows.Forms.TextBox();
+            this.BtextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.UravnLabel = new System.Windows.Forms.Label();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // AtextBox
+            // 
+            this.AtextBox.Location = new System.Drawing.Point(12, 25);
+            this.AtextBox.Name = "AtextBox";
+            this.AtextBox.Size = new System.Drawing.Size(174, 20);
+            this.AtextBox.TabIndex = 0;
+            // 
+            // BtextBox
+            // 
+            this.BtextBox.Location = new System.Drawing.Point(12, 64);
+            this.BtextBox.Name = "BtextBox";
+            this.BtextBox.Size = new System.Drawing.Size(174, 20);
+            this.BtextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Левая граница интегрирования";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Правая граница интегрирования";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(241, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Интегрируемое уравнение: ";
+            // 
+            // UravnLabel
+            // 
+            this.UravnLabel.AutoSize = true;
+            this.UravnLabel.Location = new System.Drawing.Point(395, 9);
+            this.UravnLabel.Name = "UravnLabel";
+            this.UravnLabel.Size = new System.Drawing.Size(25, 13);
+            this.UravnLabel.TabIndex = 5;
+            this.UravnLabel.Text = "???";
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(244, 48);
+            this.maskedTextBox3.Mask = "099";
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(162, 20);
+            this.maskedTextBox3.TabIndex = 8;
+            this.maskedTextBox3.ValidatingType = typeof(int);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.maskedTextBox3);
+            this.Controls.Add(this.UravnLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtextBox);
+            this.Controls.Add(this.AtextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox AtextBox;
+        private System.Windows.Forms.TextBox BtextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label UravnLabel;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+    }
+}
+
---
+++ Летняя мания (практика)/Летняя мания (практика)/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Летняя_мания__практика_
+{
+    public partial class Form1 : Form
+    {
+        private const double Eps = 0.001;
+        private bool F_try = true;
+        private double a = 0.0;
+        private double b = 0.0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double func(double x)
+        {
+            return (x);
+        }
+
+        private double simpson()
+        {
+            return 0;
+        }
+
+        private double sek(double X1, double X2, double Rez, double Y)
+        {
+            do
+            {
+                double Ex1 = 0.0, Ex2 = 0.0;
+                Ex2 = simpson(0, X2);
+                Ex1 = simpson(0, X1);
+                Y = Rez;
+                if (Ex2 == Ex1)
+                {
+                    X1 = 1;
+                }
+                if (F_try)
+                {
+                    for (int i = -1; i < 3; i++)
+                       if (simpson(0, i) == 0) return i;
+                    F_try = false;
+                }
+            } while (Math.Abs(Y - Rez) >= Eps);
+            return Rez;
+        }
+        private void starter()
+        {
+            int N = 1;
+            Stopwatch Stp = new Stopwatch();
+            Stp.Start();
+            double Otvet1 = simpson(Convert.ToDouble(AtextBox.Text), Convert.ToDouble(BtextBox.Text));
+            double Fotvet = Math.Round(Otvet1 * 1000) / 1000;
+            double X1 = -9, X2 = 10, Rez = 0.0, Y = 0.0;
+
+            double X = sek(X1, X2, Rez, Y);
+            MessageBox.Show("Интеграл = " + Fotvet + "\nДопустимая погрешность = " + Eps + "\nX = " + X);
+            Stp.Stop();
+        }
+    }
+}
