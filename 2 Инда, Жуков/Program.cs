acc89dd8fa69382864ab840d2f66a44dd80b584c
『 Enderian 』 <iggor.malakhov@gmail.com>
2020-11-19 23:21:24 +0300

    2  Индивидуалка
    2 Инда Жукова

---
+++ 2 Инда, Жуков/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace _2_Инда__Жуков
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Университет\Жуков\2 Инда, Жуков\alarm.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string s;
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+        }
+    }
+}
