c9f10753d4a34e1a5e48f45014ce18d42a4d70a6
『 Enderian 』 <iggor.malakhov@gmail.com>
2020-11-20 00:00:49 +0300

    Создан взвешенный граф
    класс

--- 2 Инда, Жуков/Program.cs
+++ 2 Инда, Жуков/Program.cs
@@ -7,17 +7,23 @@ namespace _2_Инда__Жуков
     {
         static void Main(string[] args)
         {
-            string path = @"D:\Университет\Жуков\2 Инда, Жуков\alarm.txt";
-            try
+            string path = @"D:\GitKraken\CSharp\2 Инда, Жуков\alarm.txt";
+            int n = 0;
+            StreamReader sr = File.OpenText(path);
+            string s;
+            n = Convert.ToInt32(sr.ReadLine());
+            s = sr.ReadLine();
+            string[] range = s.Split(' ');
+            int[,] Matrix = new int[n, n];
+            for (int i = 0; i < n - 1; i++)
             {
-                using (StreamReader sr = File.OpenText(path))
-                {
-                    string s;
-                    while ((s = sr.ReadLine()) != null)
-                    {
-                        Console.WriteLine(s);
-                    }
-                }
+               s = sr.ReadLine();
+               string[] forMatr = s.Split(' ');
+               int ii = Convert.ToInt32(forMatr[0]) - 1, jj = Convert.ToInt32(forMatr[1]) - 1;
+               Matrix[ii, jj] = Convert.ToInt32(forMatr[2]);
+            } 
+
+            
         }
     }
 }
