8139e62f63ab8680a794da1e045bc861608219ee
『 Enderian 』 <iggor.malakhov@gmail.com>
2020-11-20 04:29:33 +0300

    переименования

--- 2 Инда, Жуков/Program.cs
+++ 2 Инда, Жуков/Program.cs
@@ -88,7 +88,7 @@ namespace _2_Инда__Жуков
             return d;
         }
 
-        static void Prov(List<int> listing, int[,] matrix, string[] range, bool[] on_off)
+        static void Aktiv(List<int> listing, int[,] matrix, string[] range, bool[] on_off)
         {
             while(listing.Count != 0)
             {
@@ -108,7 +108,7 @@ namespace _2_Инда__Жуков
         }
         static void Main(string[] args)
         {
-            string path = @"D:\GitKraken\CSharp\2 Инда, Жуков\alarm1.txt", s;
+            string path = @"D:\GitKraken\CSharp\2 Инда, Жуков\alarm.txt", s;
 
             StreamReader sr = File.OpenText(path);
             n = Convert.ToInt32(sr.ReadLine());
@@ -130,11 +130,11 @@ namespace _2_Инда__Жуков
 
             int[] degre = new int[n];
             degre = Degreeses(matrix);
-            int[,] costs = new int[2, n];
+            int[,] values = new int[2, n];
             for (int i = 0; i < n; i++) 
             {
-                costs[0, i] = i;
-                costs[1, i] = degre[i] * Convert.ToInt32(range[i]);
+                values[0, i] = i;
+                values[1, i] = degre[i] * Convert.ToInt32(range[i]);
             }
             bool[] on_or_off = new bool[n];
             for (int i = 0; i < n; i++) on_or_off[i] = false;
@@ -143,27 +143,27 @@ namespace _2_Инда__Жуков
             {
                 for (int j = 0; j < n - i - 1; j++)
                 {
-                    if(costs[1,j] < costs[1,j + 1])
+                    if(values[1,j] < values[1,j + 1])
                     {
-                        int swap = costs[1, j];
-                        costs[1, j] = costs[1, j + 1];
-                        costs[1, j + 1] = swap;
-                        swap = costs[0, j];
-                        costs[0, j] = costs[0, j + 1];
-                        costs[0, j + 1] = swap;
+                        int swap = values[1, j];
+                        values[1, j] = values[1, j + 1];
+                        values[1, j + 1] = swap;
+                        swap = values[0, j];
+                        values[0, j] = values[0, j + 1];
+                        values[0, j + 1] = swap;
                     }
                 }
             }
 
             for (int i = 0; i < n; i++)
             {
-                int vertex = costs[0, i];
+                int vertex = values[0, i];
                 list_on.Clear();
                 list_on.Add(vertex);
                 if(!on_or_off[vertex])
                 {
                     rez.Add(vertex);
-                    Prov(list_on, matrix, range, on_or_off);
+                    Aktiv(list_on, matrix, range, on_or_off);
                 }
             }
 
